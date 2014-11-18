f608a00d3ff9b2c0d1bc61b7ef31bedabe75c3ea
Sahibjot Saggu <sahibjot@live.ca>
2014-11-18 12:11:20 -0500

    updated landing page

--- loading_script.js
+++ loading_script.js
@@ -1,3 +1,18 @@
+function adjustText() {
+	if ($(window).width() <= 600) {
+		$("#title").html("city-<br>kit");
+		$("#title").css("font-size", $("#title").height());
+		$("#title").css("top", ($(window).height()/2) - ($("#title").height()) + "px");
+	}
+	else {
+		$("#title").html("citykit");
+		$("#title").css("font-size", $("#title").height());
+		//$("#loader_title").css("font-size", $("#loader_title").height());
+		$("#title").css("top", ($(window).height()/2) - ($("#title").height()/2) + "px");
+		$("#slogan").css({'top' : '65%'});
+	}
+}
+
 $(document).ready(function() {	
 	//var get_window_height = $(window).height();
 	//$("#loading_bar").css("top", get_window_height/2 + "px");
@@ -6,24 +21,14 @@ $(document).ready(function() {
 	$("#loader_title").delay(3000).fadeOut();
 	$("#top").delay(3000).animate({'height' : '0%'});
 	$("#bottom").delay(3000).animate({'height' : '0%'});*/
-	$("#title").html("citykit");
 	$("#title").css("font-size", $("#title").height());
-	$("#loader_title").css("font-size", $("#loader_title").height());
-	$("#title").css("top", ($(window).height()/2) - ($("#title").height()/2) + "px");	
+	$("#loader_title").css("font-size", $("#loader_title").height());	
+	$("#slogan").css({'top' : '10%', 'background' : '#333'});
+	adjustText();
 
 	$(window).resize(function() {
 		//$("#loading_bar").css("top", get_window_height/2 + "px");
 		/*$("#title").html($(window).width());*/
-		if ($(window).width() <= 516) {
-			$("#title").html("city-<br>kit");
-			$("#title").css("font-size", $("#title").height());
-			$("#title").css("top", ($(window).height()/2) - ($("#title").height()) + "px");
-		}
-		else {
-			$("#title").html("citykit");
-			$("#title").css("font-size", $("#title").height());
-			//$("#loader_title").css("font-size", $("#loader_title").height());
-			$("#title").css("top", ($(window).height()/2) - ($("#title").height()/2) + "px");
-		}
+		adjustText();
 	});
 });
